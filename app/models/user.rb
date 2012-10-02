7257819ade19869d9f4ce11e607ad37da1618bd9
budhram <budhram@vertisinfotech.com>
2012-10-02 21:14:51 +0530

    Resolved 500 error in user creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model  
-  attr_accessible :birth_date, :first_name, :last_name, :user_name, :mobile, :phone, :is_active, :joining_date
-  has_many :leaves 
+  attr_accessible :birth_date, :first_name, :last_name, :user_name, :mobile, :phone, :is_active, :joining_date  
   
 end
