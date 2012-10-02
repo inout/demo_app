320fad3e24637af99e758bd0bdd6c17d7ccf4c35
budhram <budhram@vertisinfotech.com>
2012-10-03 00:22:33 +0530

    Added following changes: - Created dummy leave request page. - Updated route file for request_leave. - Added home directory and its view files.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   active_scaffold :user do |config|
     config.columns = [:first_name, :last_name, :user_name, :mobile, :phone, :birth_date, :joining_date, :is_active, :created_at, :updated_at]
   end
+  
+  def request_leave
+    @user_id = params[:id]
+    
+    
+  end
 end 
