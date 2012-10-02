b6911045b894e4650f59272b7e164a99b0110232
budhram <budhram@vertisinfotech.com>
2012-10-02 21:12:32 +0530

    Changed user controller, model and performed migration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
-  active_scaffold :user do |conf|
+  active_scaffold :user do |config|
+    config.columns = [:first_name, :last_name, :user_name, :mobile, :phone, :birth_date, :joining_date, :is_active, :created_at, :updated_at]
   end
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+
+  # Setup accessible (or protected) attributes for your model  
+  attr_accessible :birth_date, :first_name, :last_name, :user_name, :mobile, :phone, :is_active, :joining_date
+  has_many :leaves 
+  
 end
