124dd7807a761b9998dafaaf6102cd4238374fba
budhram <budhram@vertisinfotech.com>
2012-10-13 19:48:15 +0530

    Added changes to views to work as expected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,5 @@ class HomeController < ApplicationController
   def index
   end
   
+  before_filter :authenticate_user!
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   active_scaffold :user do |config|
     config.columns = [:first_name, :last_name, :user_name, :email, :mobile, :phone, :birth_date, :joining_date, :is_active, :created_at, :updated_at]
+    
+    config.actions.exclude :create
   end
   
   def request_leave
-    @user_id = params[:id]
-    
-    
+    @user_id = params[:id]    
   end
+  
   before_filter :authenticate_user!
 end 
