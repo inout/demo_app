a312cc14d432e377e6b526e18f6f2e396c203a52
budhram <budhram@vertisinfotech.com>
2012-10-13 19:32:33 +0530

    Added necessary ldap related changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   active_scaffold :user do |config|
-    config.columns = [:first_name, :last_name, :user_name, :mobile, :phone, :birth_date, :joining_date, :is_active, :created_at, :updated_at]
+    config.columns = [:first_name, :last_name, :user_name, :email, :mobile, :phone, :birth_date, :joining_date, :is_active, :created_at, :updated_at]
   end
   
   def request_leave
